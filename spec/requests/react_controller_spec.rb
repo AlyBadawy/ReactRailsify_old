f583c73cccc727f0652bf27f2d9f95cf59d789f2
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-04 00:59:11 -0500

    Mount react application

--- spec/requests/react_controller_spec.rb
+++ spec/requests/react_controller_spec.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Reacts" do
-  describe "GET /index" do
+RSpec.describe ReactController do
+  describe "GET /" do
     it "returns http success" do
-      get "/react/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
