aeca65bf8198c8be180b09e495f7e51948311eae
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-20 10:44:04 -0400

    PaperTrail to User (#29)
    * PaperTrail to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_paper_trail
+
   include Devise::JWT::RevocationStrategies::Allowlist
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
